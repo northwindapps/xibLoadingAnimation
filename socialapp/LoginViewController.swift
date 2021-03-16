6e80867cd19246a0a69b0e4143167be4d576e248
Yujin Yano <northwindapps@gmail.com>
2021-03-16 10:29:01 +0900

    async functions

--- socialapp/LoginViewController.swift
+++ socialapp/LoginViewController.swift
@@ -6,19 +6,36 @@
 //
 
 import UIKit
+let HOST_IP = "northwindapps.com"
+let session = URLSession.shared
 
-class LoginViewController: UIViewController {
+class LoginViewController: ViewController {
 
     var loadingview1:loadingview!
     var timerCounter = 0
+  
+    
+    @IBOutlet weak var resultview: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
 
-        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.timerUpdate), userInfo: nil, repeats: true)
+        let timer_loading = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.timerUpdate), userInfo: nil, repeats: true)
         
         // Do any additional setup after loading the view.
+        
+        fetchquestion(token: "notoken"){
+            data in
+            timer_loading.invalidate()
+            if self.loadingview1 != nil{
+                self.loadingview1.removeFromSuperview()
+            }
+            
+            self.resultview.text = "Done"
+            print(data)
+            
+        }
     }
     
 
@@ -45,17 +62,17 @@ class LoginViewController: UIViewController {
         case 0:
          
             loadingview1.imageview.image = UIImage(named:"counter1")
-            loadingview1.imageview.contentMode = .scaleAspectFit
+//            loadingview1.imageview.contentMode = .scaleAspectFit
             break
         case 1:
          
             loadingview1.imageview.image = UIImage(named:"counter2")
-            loadingview1.imageview.contentMode = .scaleAspectFit
+//            loadingview1.imageview.contentMode = .scaleAspectFit
             break
         case 2:
            
             loadingview1.imageview.image = UIImage(named:"counter3")
-            loadingview1.imageview.contentMode = .scaleAspectFit
+//            loadingview1.imageview.contentMode = .scaleAspectFit
             break
         default:
             break
@@ -68,10 +85,55 @@ class LoginViewController: UIViewController {
     
     @objc func timerUpdate(){
         timerCounter += 1
-
         openloadingview()
-        
-        
     }
+    
+    //https://stackoverflow.com/questions/25203556/returning-data-from-async-call-in-swift-function
+    
+    //https://medium.com/@dhavalkansara51/completion-handler-in-swift-with-escaping-and-nonescaping-closures-1ea717dc93a4
+    
+    func fetchquestion(token:String,completion: @escaping (NSArray)-> Void){
+           
+           let Url = "https://" + HOST_IP + "/api/post"
+                   print(Url)
+                 let serviceUrl = URL(string: Url)
+                 var request = URLRequest(url: serviceUrl!)
+                 request.setValue("Bearer " + token, forHTTPHeaderField:  "Authorization" )
+                 request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+                 request.setValue("XMLHttpRequest", forHTTPHeaderField: "X-Requested-With")
+                 
+                 request.httpMethod = "GET"
+                
+           
+        session.dataTask(with: request as URLRequest, completionHandler:{ (data, response, error) in
+                
+                     if let data = data {
+                         do {
+                           let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+
+                            var output = NSMutableArray()
+                            let check = json["data"] as? NSArray
+                            
+                            if check != nil{
+                                for i in 0..<check!.count {
+                                    output.add(check![i])
+                                }
+                            }
+                            
+                            
+                            DispatchQueue.main.async{
+                                completion(output)
+                            }
+                            
+                            
+                         } catch {
+                           print(error)
+    
+                         }
+                     }
+                     }).resume()
+       }
+    
+    
 
 }
